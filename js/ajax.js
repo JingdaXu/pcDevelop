1ec8bfc9c642fa9b77e010595e5c912373c59849
Archer <3130705067@qq.com>
2017-09-29 16:02:32 +0800

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,21 @@
+//ajax提交form表单的方式
+$('#formAddHandlingFee').submit(function() {
+        var AjaxURL= "../OrderManagement/AjaxModifyOrderService.aspx";
+        alert($('#formAddHandlingFee').serialize());
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            url: AjaxURL + '?Action=' + 'SubmitHandlingFee' + '&OrderNumber=' + $.trim($("#<%=this.txtOrderNumber.ClientID %>").val()),
+            data: $('#formAddHandlingFee').serialize(),
+            success: function (data) {
+                var strresult=data;
+                alert(strresult);
+                //加载最大可退金额
+                $("#spanMaxAmount").html(strresult);
+            },
+            error: function(data) {
+                alert("error:"+data.responseText);
+            }
+        });
+    }
+);
